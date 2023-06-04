c798364e8a5cb2e9cb51a4c0b620e63c875886c2
AlexT97x <alextinella@gmail.com>
2023-06-04 19:14:05 +0200

    implemetazione timer e diplay none

--- script/script.js
+++ script/script.js
@@ -12,27 +12,43 @@ function generateUniqueRandomNumbers() {
     return numeriRandom;
   }
   
-  let numeriRandom = generateUniqueRandomNumbers();
-  console.log(numeriRandom);
+  function showRandomNumbers() {
+    let numeriRandom = generateUniqueRandomNumbers();
+    let numbersContainer = document.getElementById('numbersContainer');
+    
   
-  console.log(numeriRandom);
-let userNumbers = [];
-
-for (let i = 0; i < 5; i++) {
-  let userInput = parseInt(prompt("Inserisci un numero:"));
-  userNumbers.push(userInput);
-}
-
-let matchingNumbers = [];
-
-for (let i = 0; i < 5; i++) {
-  if (numeriRandom.includes(userNumbers[i])) {
-    matchingNumbers.push(userNumbers[i]);
+    setTimeout(function() {
+      numbersContainer.style.display = 'none';
+      askUserForNumbers(numeriRandom);
+    }, 5000);
+  
+    console.log('Numeri casuali: ', numeriRandom);
   }
-}
-
-if (matchingNumbers.length > 0) {
-  alert("Hai indovinato i seguenti numeri: " + matchingNumbers.join(", "));
-} else {
-  alert("Non hai indovinato nessun numero.");
-}
+  
+  function askUserForNumbers(numeriRandom) {
+    let userNumbers = [];
+  
+    for (let i = 0; i < 5; i++) {
+      let userInput = parseInt(prompt('Inserisci un numero:'));
+      userNumbers.push(userInput);
+    }
+  
+    let matchingNumbers = [];
+  
+    for (let i = 0; i < 5; i++) {
+      if (numeriRandom.includes(userNumbers[i])) {
+        matchingNumbers.push(userNumbers[i]);
+      }
+    }
+  
+    if (matchingNumbers.length > 0) {
+      alert('Hai indovinato i seguenti numeri: ' + matchingNumbers.join(', '));
+    } else {
+      alert('Non hai indovinato nessun numero.');
+    }
+  
+    console.log('Numeri inseriti dall\'utente: ', userNumbers);
+  }
+  
+  showRandomNumbers();
+  
